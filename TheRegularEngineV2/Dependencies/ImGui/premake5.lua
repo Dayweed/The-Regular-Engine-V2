0e7ae7490fb119d6937047f06f657076b2f4519b
Pancakes <90077766+Pancakes99@users.noreply.github.com>
2023-04-12 22:44:21 +0800

    Imgui premake file

--- TheRegularEngineV2/Dependencies/ImGui/premake5.lua
+++ TheRegularEngineV2/Dependencies/ImGui/premake5.lua
@@ -8,17 +8,8 @@ project "ImGui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"include/Imgui/**.h,
+		"include/Imgui/**.cpp,
 	}
 
 	filter "system:windows"
