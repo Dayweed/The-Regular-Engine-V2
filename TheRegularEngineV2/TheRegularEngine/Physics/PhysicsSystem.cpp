3d7303932c28d019926bee2432ed8ecff16f9945
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2023-08-16 15:17:08 +0800

    physx test is back in!

--- TheRegularEngineV2/TheRegularEngine/Physics/PhysicsSystem.cpp
+++ TheRegularEngineV2/TheRegularEngine/Physics/PhysicsSystem.cpp
@@ -6,16 +6,82 @@ namespace TRE
 {
 	PhysicsSystem::PhysicsSystem()
 	{
-		std::cout << "Physics System Init" << std::endl;
+		std::cout << "Physics System Constructed\n";
 	}
 
 	PhysicsSystem::~PhysicsSystem()
 	{
+		std::cout << "Physics System Destroyed\n";
+	}
 
+	bool PhysicsSystem::TESTInit()
+	{
+		std::cout << "Physics System TESTInit\n";
+
+
+#if HAHA_PHYSX_TEST
+		//Create foundation is similar to initializing the scene
+		m_Foundation = PxCreateFoundation(PX_PHYSICS_VERSION, m_Allocator, m_ErrorCallback);
+
+		printf("PhysX Version: %d.%d.%d\n",
+			PX_PHYSICS_VERSION_MAJOR, PX_PHYSICS_VERSION_MINOR, PX_PHYSICS_VERSION_BUGFIX);
+
+#if HAHA_PHYSX_PVD
+		//PVD is like a debugger for the physics (leave off for submission)
+		m_Pvd = PxCreatePvd(*m_Foundation);
+		physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+		m_Pvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+#endif
+
+		m_Physics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_Foundation, physx::PxTolerancesScale(), true, m_Pvd);
+
+		physx::PxSceneDesc sceneDesc(m_Physics->getTolerancesScale());
+		sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+		m_Dispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+
+		//A cpu thread for the scene
+		sceneDesc.cpuDispatcher = m_Dispatcher;
+
+		//A thread that will do collision management
+		sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+		m_Scene = m_Physics->createScene(sceneDesc);
+
+#if HAHA_PHYSX_PVD
+		physx::PxPvdSceneClient* pvdClient = m_Scene->getScenePvdClient();
+		if (pvdClient)
+		{
+			pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+			pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+			pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+		}
+#endif
+
+		//Create material gives the object a static, dynamic and restitution.
+		m_Material = m_Physics->createMaterial(0.5f, 0.5f, 0.6f);
+
+		//Static object creation
+		physx::PxRigidStatic* groundPlane = PxCreatePlane(*m_Physics, physx::PxPlane(0, 1, 0, 0), *m_Material);
+		m_Scene->addActor(*groundPlane);
+
+		//Create 5 stacks of boxes
+		for (physx::PxU32 i = 0; i < 5; i++)
+			createStack(physx::PxTransform(physx::PxVec3(0, 0, m_stackZ -= 10.0f)), 10, 2.0f);
+#endif
+		return m_IsInitialized = true;
 	}
 
 	void PhysicsSystem::Update()
 	{
+		//if (!m_IsInitialized) TESTInit();
+		// makes a non-void function only run once
+		// without any if branches, using short-circuiting! :D
+		m_IsInitialized || TESTInit();
+
+#if HAHA_PHYSX_TEST
+		m_Scene->simulate(1.0f / 60.0f);
+		m_Scene->fetchResults(true);
+#endif
+
 		std::cout << "PhysicsUpdate: Printing useless data m_PosX...---------------------\n";
 		for (GO obj : _ecs_manager->GetGO<Transform>())
 		{
@@ -28,9 +94,34 @@ namespace TRE
 	{
 
 	}
-	
+
 	void PhysicsSystem::Shutdown()
 	{
-		std::cout << "Physics System Shutdown" << std::endl;
+		std::cout << "Physics System Shutdown\n";
 	}
+
+#if HAHA_PHYSX_TEST
+	//This function creates a stack of boxes
+	void PhysicsSystem::createStack(const physx::PxTransform& t, physx::PxU32 size, physx::PxReal halfExtent)
+	{
+		//Create the shape of the box
+		// physx::PxShape* shape = m_Physics->createShape(physx::PxBoxGeometry(halfExtent, halfExtent, halfExtent), *m_Material);
+		physx::PxShape* shape = m_Physics->createShape(physx::PxSphereGeometry(halfExtent), *m_Material);
+		//Create the boxes stacked 
+		for (physx::PxU32 i = 0; i < size; i++)
+		{
+			for (physx::PxU32 j = 0; j < size - i; j++)
+			{
+				//PxTransform is the position represented in a vector multiplied by its halfExtents
+				physx::PxTransform localTm(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 + 1), 0) * halfExtent);
+				physx::PxRigidDynamic* body = m_Physics->createRigidDynamic(t.transform(localTm));
+				body->attachShape(*shape);
+				physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+				m_Scene->addActor(*body);
+			}
+		}
+		//Release shape
+		shape->release();
+	}
+#endif
 }
--- TheRegularEngineV2/TheRegularEngine/Physics/PhysicsSystem.h
+++ TheRegularEngineV2/TheRegularEngine/Physics/PhysicsSystem.h
@@ -1,18 +1,46 @@
 #pragma once
 #include "Core/System.h"
 
+#define HAHA_PHYSX_TEST 0
+#if HAHA_PHYSX_TEST
+#include "PhysX/PxPhysicsAPI.h"
+#define HAHA_PHYSX_PVD 0
+#endif
+
 namespace TRE
 {
 	class PhysicsSystem : public System
 	{
-		public:
-			PhysicsSystem();
-			~PhysicsSystem();
+	public:
+		PhysicsSystem();
+		~PhysicsSystem();
 
-			void Update() override;
-			void RenderImgui() override;
-			void Shutdown() override;
+		bool TESTInit();
+		void Update() override;
+		void RenderImgui() override;
+		void Shutdown() override;
 
-		private: // test comment
+	private:
+
+		// Systems aren't meant to have variables, Components are.
+		// But I need variables that all components should be able to access, but
+		// they cannot be in a System...hmm...
+
+		bool m_IsInitialized = false;
+
+#if HAHA_PHYSX_TEST
+		physx::PxDefaultAllocator		m_Allocator;
+		physx::PxDefaultErrorCallback	m_ErrorCallback;
+		physx::PxFoundation*			m_Foundation = nullptr;
+		physx::PxPhysics*				m_Physics = nullptr;
+		physx::PxDefaultCpuDispatcher*	m_Dispatcher = nullptr;
+		physx::PxScene*					m_Scene = nullptr;
+		physx::PxMaterial*				m_Material = nullptr;
+		physx::PxPvd*					m_Pvd = nullptr;
+		physx::PxReal					m_stackZ = 10.0f;
+
+		//This function creates a stack of boxes
+		void createStack(const physx::PxTransform& t, physx::PxU32 size, physx::PxReal halfExtent);
+#endif
 	};
 }
