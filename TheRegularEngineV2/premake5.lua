37b92a16dd8c60a28b59a5eb427e897d96d7ae7b
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2023-09-04 19:49:34 +0800

    reduced physx cooking warnings
    hope i won't need that file!

--- TheRegularEngineV2/premake5.lua
+++ TheRegularEngineV2/premake5.lua
@@ -88,7 +88,7 @@ project "TheRegularEngine"
 		"%{Library.PhysX_Extension}",
 		"%{Library.PhysX_Character}",
 		"%{Library.PhysX_Common}",
-		"%{Library.PhysX_Cooking}",
+		--"%{Library.PhysX_Cooking}",
 		"%{Library.PhysX_Pvd}",
 		"%{Library.PhysX_TaskStatic}",
 		"%{Library.PhysX_VehicleStatic}",
