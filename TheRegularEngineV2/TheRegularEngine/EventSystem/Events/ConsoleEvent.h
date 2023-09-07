40f8424d272d643983fdff69d756788d797b1907
Dayweed <tan.d@digipen.edu>
2023-09-08 01:13:50 +0800

    Comment on how to use event

--- TheRegularEngineV2/TheRegularEngine/EventSystem/Events/ConsoleEvent.h
+++ TheRegularEngineV2/TheRegularEngine/EventSystem/Events/ConsoleEvent.h
@@ -10,4 +10,6 @@ namespace TRE
 		std::string m_Msg;
 		ConsoleDebugEvent(std::string&& msg) : m_Msg(std::move(msg)) {}
 	};
+	//To use the event just include this file and publish the event like this:
+	//EventHandler::getEventHandlerInstance().Publish(ConsoleDebugEvent{ "Testing Key inputs here" });
 }
