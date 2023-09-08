a79c314c5e371afed4f8192f47781cda502ac776
Toby IO <toby-io@hotmail.com>
2023-09-08 22:51:45 +0800

    Scattering sparks of thought energy Deliver me and carry me away Here in my kingdom I am your Lord I order you to cower and pray

--- TheRegularEngineV2/TheRegularEngine/Core/SceneManager.h
+++ TheRegularEngineV2/TheRegularEngine/Core/SceneManager.h
@@ -1,4 +1,15 @@
 #pragma once
+/*!
+	@file		SceneManager.h
+	@author		Isaiah Lim (Code Contribution 100%)
+	@email		lim.i@digipen.edu
+	@date		08/09/2023
+	@brief		Handles all the front end of handling scenes
+
+	Copyright (C) 2023 DigiPen Institute of Technology.
+	Reproduction or disclosure of this file or its contents without the
+	prior written consent of DigiPen Institute of Technology is prohibited.
+************************************************************************/
 
 #define SCENE_DEFAULT_NAME "New Scene"
 
@@ -15,9 +26,44 @@ namespace TRE
 			return instance;
 		}
 
+		/* !
+		@function		NewScene
+		@author			Isaiah Lim (lim.i@digipen.edu)
+
+		@brief			Creates a new scene
+
+		[Warning]		Will delete all the entities! Remember to save first!
+		*//*__________________________________________________________________________*/
 		void NewScene();
+
+		/* !
+		@function		LoadScene
+		@author			Isaiah Lim (lim.i@digipen.edu)
+
+		@params			sceneName	Name of the scene to be load as
+
+		@brief			Load a new scene based on the scene name
+
+		[Warning]		Will delete all the entities! Remember to save first!
+		*//*__________________________________________________________________________*/
 		void LoadScene(std::string sceneName);
+
+		/* !
+		@function		SaveSceneAs
+		@author			Isaiah Lim (lim.i@digipen.edu)
+
+		@params			sceneName	Name of the scene to be save as
+
+		@brief			Save the scene based on the scene name
+		*//*__________________________________________________________________________*/
 		void SaveSceneAs(std::string sceneName);
+
+		/* !
+		@function		SaveScene
+		@author			Isaiah Lim (lim.i@digipen.edu)
+
+		@brief			Save the scene based on the current scene name
+		*//*__________________________________________________________________________*/
 		void SaveScene();
 
 	private:
