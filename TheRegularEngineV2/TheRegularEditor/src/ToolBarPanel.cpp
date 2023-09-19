7a4d326383097e3a0ef8c885f084a6485b35d7e2
Pancakes <jerelpeck99@gmail.com>
2023-09-20 00:32:29 +0800

    Hide the validation error. Waste my time cb

--- TheRegularEngineV2/TheRegularEditor/src/ToolBarPanel.cpp
+++ TheRegularEngineV2/TheRegularEditor/src/ToolBarPanel.cpp
@@ -26,7 +26,7 @@ namespace TRE
 		//How to pad in imgui
 		ImGui::SameLine(ImGui::GetContentRegionAvail().x/2 - 25);
 
-		if (ImGui::ImageButton(nullptr, ImVec2(20, 20), ImVec2(0, 0), ImVec2(0, 0), 0))
+	/*	if (ImGui::ImageButton(nullptr, ImVec2(20, 20), ImVec2(0, 0), ImVec2(0, 0), 0))
 		{
 			TRE_CORE_INFO("Play Button Pressed");
 		}
@@ -43,7 +43,7 @@ namespace TRE
 		if (ImGui::ImageButton(nullptr, ImVec2(20, 20), ImVec2(0, 0), ImVec2(0, 0), 0))
 		{
 			TRE_CORE_INFO("Work Button Pressed");
-		}
+		}*/
 
 		ImGui::End();
 	}
