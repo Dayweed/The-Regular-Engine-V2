1f8ab38c4d8fedd7a0eaef9c34b297ca5d448192
Dayweed <tan.d@digipen.edu>
2023-09-21 15:29:49 +0800

    This is toby push

--- TheRegularEngineV2/TheRegularEditor/src/ProfilerPanel.cpp
+++ TheRegularEngineV2/TheRegularEditor/src/ProfilerPanel.cpp
@@ -25,7 +25,12 @@ namespace TRE
 		ImGui::Begin("Profiler");
 		std::stringstream Fps;
 		Fps << 1/Engine::GetInstance().GetWindow()->GetDeltaTime() << "FPS\n";
-		ImGui::TextUnformatted(Fps.str().c_str());
+		//Profiler::GetTimers()
+		//ImGui::PlotLines()
+		std::stringstream physics;
+		physics << Profiler::Instance().GetTimers()[typeid(MeshRendererSystem).name()]->GetTime() << "ms\n";
+		ImGui::TextUnformatted(physics.str().c_str());
+
 		ImGui::End();
 	}
 
