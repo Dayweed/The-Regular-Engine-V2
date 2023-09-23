9fa9b667bab089fab6246ec4f09888e722cb1ffb
Pancakes <jerelpeck99@gmail.com>
2023-09-23 21:09:46 +0800

    temp

--- TheRegularEngineV2/TheRegularEngine/Graphics/Shader.h
+++ TheRegularEngineV2/TheRegularEngine/Graphics/Shader.h
@@ -24,7 +24,7 @@ namespace TRE
 
 		public:
 			VkPipelineShaderStageCreateInfo GetPipelineShaderInfo();
-			const std::unordered_map<std::string, VkWriteDescriptorSet>& GetWriteDescriptorSets();
+			std::unordered_map<std::string, VkWriteDescriptorSet> GetWriteDescriptorSets();
 
 		private:
 			VkPipelineShaderStageCreateInfo m_PipelineShaderCreateInfo;
