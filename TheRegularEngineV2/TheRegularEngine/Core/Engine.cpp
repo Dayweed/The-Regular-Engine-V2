5624e932ffc7f83ec71f24e2f7c192e680b841af
zhengrongg <zhengrong29@gmail.com>
2023-09-28 17:12:51 +0800

    orite plane

--- TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
@@ -101,7 +101,9 @@ namespace TRE
 		auto textureHandle2 = Resource::GetGUIDFromHex("d3464713e4f44bee"); //normal
 		auto textureHandle3 = Resource::GetGUIDFromHex("8a0c8bee2a64d76b"); //roughness
 		auto textureHandle4 = Resource::GetGUIDFromHex("13392e8301ebb46"); //AO
-		auto skullHandle = Resource::GetGUIDFromHex("b1d2057915001876");
+		auto textureHandle10 = Resource::GetGUIDFromHex("c8749664b7fae78b"); //white
+		auto skullHandle = Resource::GetGUIDFromHex("b1d2057915001876"); //skull
+		auto planeHandle = Resource::GetGUIDFromHex("b262c8535c88eff7"); //plane
 		auto vertHandle = 3;
 		auto fragHandle = 4;
 		auto AnimationVertHandle = 5;
@@ -137,7 +139,6 @@ namespace TRE
 		vkt4->SetHandle(textureHandle4);
 		ResourceManager::Instance().AddResource(std::move(vkt4));
 
-
 		//Animation Textures//
 		//Texture::RunCompiler("../Assets/9c6509635ee2d750.desc");
 		std::unique_ptr<VulkanTexture> vkt5 = std::make_unique<VulkanTexture>("../Resources/9c6509635ee2d750.DDS");
@@ -169,6 +170,16 @@ namespace TRE
 		std::unique_ptr<RenderObject> ro = std::make_unique<RenderObject>("../Resources/b1d2057915001876.geom");
 		ro->SetHandle(skullHandle);
 		ResourceManager::Instance().AddResource(std::move(ro));
+
+		//Geom::RunCompiler("../Assets/b262c8535c88eff7.desc");
+		std::unique_ptr<RenderObject> plane = std::make_unique<RenderObject>("../Resources/b262c8535c88eff7.geom");
+		plane->SetHandle(planeHandle);
+		ResourceManager::Instance().AddResource(std::move(plane));
+
+		//Texture::RunCompiler("../Assets/c8749664b7fae78b.desc");
+		std::unique_ptr<VulkanTexture> vkt10 = std::make_unique<VulkanTexture>("../Resources/c8749664b7fae78b.DDS");
+		vkt10->SetHandle(textureHandle10);
+		ResourceManager::Instance().AddResource(std::move(vkt10));
 
 		std::unique_ptr<Shader>vert = ShaderCompiler::CompileShader("../Resources/Shaders/PBR.vert");
 		vert->SetHandle(vertHandle);
@@ -213,13 +224,13 @@ namespace TRE
 		mat1->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle4));
 		ResourceManager::Instance().AddResource(std::move(mat1));
 
-	/*	std::unique_ptr<Material> mat2 = std::make_unique<Material>(VertShader, FragShader);
+		std::unique_ptr<Material> mat2 = std::make_unique<Material>(VertShader, FragShader);
 		mat2->SetHandle(matHandle2);
-		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle));
-		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle2));
-		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle3));
-		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle4));
-		ResourceManager::Instance().AddResource(std::move(mat2));*/
+		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle10));
+		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle10));
+		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle10));
+		mat2->SetTextures(ResourceManager::Instance().GetResource<VulkanTexture>(textureHandle10));
+		ResourceManager::Instance().AddResource(std::move(mat2));
 
 		auto transformSystem = ECSSystemManager::Instance().GetSystem<TransformSystem>();
 		auto meshRendererSystem = ECSSystemManager::Instance().GetSystem<MeshRendererSystem>();
@@ -243,14 +254,14 @@ namespace TRE
 		test->AddComponent<SphereCollider>();
 		test->AddComponent<Rigidbody>();
 
-		/*Entity test2 = ECSManager::Instance().CreateEntity();
-		test2->GetComponent<Properties>().m_Name = "Test2";
-		transformSystem->SetPosition(test2, glm::vec3(0.f, 20.f, 180.f));
-		transformSystem->SetScale(test2, glm::vec3(0.5f, 0.5f, 0.5f));
-		transformSystem->SetRotation(test2, glm::vec3(0, 180.f, 0));
-		test2->AddComponent<MeshRenderer>();
-		meshRendererSystem->SetMeshRenderer(test2, ResourceManager::Instance().GetResource<RenderObject>(skullHandle));
-		meshRendererSystem->SetMaterial(test2, ResourceManager::Instance().GetResource<Material>(matHandle2));*/
+		Entity planeCollider = ECSManager::Instance().CreateEntity();
+		planeCollider->GetComponent<Properties>().m_Name = "Plane collider";
+		transformSystem->SetPosition(planeCollider, glm::vec3(0.f, -35.f, 100.f));
+		transformSystem->SetScale(planeCollider, glm::vec3(10.f, 10.f, 10.f));
+		transformSystem->SetRotation(planeCollider, glm::vec3(0, 0, 0));
+		planeCollider->AddComponent<MeshRenderer>();
+		meshRendererSystem->SetMeshRenderer(planeCollider, ResourceManager::Instance().GetResource<RenderObject>(planeHandle));
+		meshRendererSystem->SetMaterial(planeCollider, ResourceManager::Instance().GetResource<Material>(matHandle2));
 
 		Entity cam = ECSManager::Instance().CreateEntity();
 		cam->GetComponent<Properties>().m_Name = "cam";
@@ -258,18 +269,6 @@ namespace TRE
 		cameraSystem->SetIsMainCamera(cam, true);
 		cam->AddComponent<AudioListener>();
 		audioSystem->SetListenerPosition(cam);
-
-		//Entity audio = ECSManager::Instance().CreateEntity();
-		//audio->AddComponent<Audio>();
-
-		//ECSSystemManager::Instance().GetSystem<CameraSystem>()->SetFocalPoint(cam, test->GetComponent<Transform>().m_Position);
-		// _system_manager->GetSystem<PhysicsSystem>()->ConstructSphereCollider(test2, { 4, 10, 4 }, 2);
-		//ECSSystemManager::Instance().GetSystem<AudioSystem>()->CompileAudio(audio);
-
-		//SceneManager::Instance().SaveSceneAs("../Scenes/DemoScene.json");
-
-		//SceneManager::Instance().NewScene();
-		//SceneManager::Instance().LoadScene("../Scenes/DemoScene.json");
 	}
 }
 #pragma endregion TO DELETE TEST
--- TheRegularEngineV2/TheRegularEngine/Graphics/Renderer.h
+++ TheRegularEngineV2/TheRegularEngine/Graphics/Renderer.h
@@ -23,7 +23,7 @@ namespace TRE
 		glm::vec3 m_LightPosition{ 0.f, 0.f, 0.f}; //Light position for now will be the camera in world space
 		alignas(16) glm::vec4 m_LightColor{ 1.f, 1.f, 1.f, 100.f }; //Light color, w for intensity
 		glm::vec4 m_CameraPosition{0.f, 0.f, 0.f, 1.f}; //Camera position in world space
-		glm::vec4 m_LightDirection = glm::vec4(glm::normalize(glm::vec3(1.0f, 0.f, 1.f)), 1.f);
+		glm::vec4 m_LightDirection = glm::vec4(glm::normalize(glm::vec3(1.0f, -1.f, 1.f)), 1.f);
 	};
 
 	struct AnimationUBO
