8ed4bcb03bf1cbce184052c56961e6f10ff32285
Toby IO <toby-io@hotmail.com>
2023-10-13 10:40:36 +0800

    I forgor how rotation works :clown:

--- TheRegularEngineV2/TheRegularEngine/Core/Transform.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/Transform.cpp
@@ -106,7 +106,6 @@ namespace TRE
 		Transform& transform = go.get()->GetComponent<Transform>();
 		glm::vec3 rotDiff = rotation - transform.m_OldRotation;
 		transform.m_Rotation = rotation;
-		transform.m_OldRotation = rotation;
 		transform.m_IsDirty = true;
 
 		// Update Children Rotation
@@ -116,9 +115,44 @@ namespace TRE
 			glm::vec3 childRot = childTransform.m_Rotation;
 			SetRotation(obj, childRot + rotDiff);
 			// Update Position
-			glm::vec4 localPos{ childTransform.m_Position - transform.m_Position, 0 };
-			SetPosition(obj, transform.m_Position + glm::vec3{ transform.GetModelMatrix() * localPos });
+			if (transform.m_Rotation != transform.m_OldRotation)
+			{
+				glm::vec4 localPos{ childTransform.m_Position - transform.m_Position, 1 };
+				const glm::vec3 scale = {1, 1, 1};//(transform.m_Scale);
+				const glm::vec3 rotation = glm::radians(transform.m_Rotation);
+				const float c3 = glm::cos(rotation.z);
+				const float s3 = glm::sin(rotation.z);
+				const float c2 = glm::cos(rotation.x);
+				const float s2 = glm::sin(rotation.x);
+				const float c1 = glm::cos(rotation.y);
+				const float s1 = glm::sin(rotation.y);
+				glm::mat4 rotMat
+				{
+					{
+						scale.x * (c1 * c3 + s1 * s2 * s3),
+						scale.x * (c2 * s3),
+						scale.x * (c1 * s2 * s3 - c3 * s1),
+						0.0f,
+					},
+					{
+						scale.y * (c3 * s1 * s2 - c1 * s3),
+						scale.y * (c2 * c3),
+						scale.y * (c1 * c3 * s2 + s1 * s3),
+						0.0f,
+					},
+					{
+						scale.z * (c2 * s1),
+						scale.z * (-s2),
+						scale.z * (c1 * c2),
+						0.0f,
+					},
+					{0, 0, 0, 1.0f}
+				};
+				SetPosition(obj, transform.m_Position + glm::vec3{ localPos * rotMat });
+			}
 		}
+
+		transform.m_OldRotation = rotation;
 	}
 
 	void TransformSystem::SetScale(Entity& go, const glm::vec3& scale)
