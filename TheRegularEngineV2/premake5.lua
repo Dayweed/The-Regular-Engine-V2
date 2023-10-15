d93e9e7f3c6d8d69317e23c98967e4b7d3cfeb79
Pancakes <jerelpeck99@gmail.com>
2023-10-15 14:27:59 +0800

    Remove shadercompiler from build

--- TheRegularEngineV2/premake5.lua
+++ TheRegularEngineV2/premake5.lua
@@ -23,7 +23,7 @@ include "Dependencies/Math"
 -- include "Dependencies/CompilerLib"
 -- include "Compilers/TextureCompiler"
 -- include "Compilers/GeomCompiler"
- include "Compilers/ShaderCompiler"
+-- include "Compilers/ShaderCompiler"
 group ""
 
 project "TheRegularEngine"
