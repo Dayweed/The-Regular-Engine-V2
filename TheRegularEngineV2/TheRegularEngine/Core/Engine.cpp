a0bf1ba1522294932d63f85a5ae4273a1a450cf3
Toby IO <toby-io@hotmail.com>
2023-10-20 18:39:36 +0800

    Commented out the thing that keep saving scenes for now...

--- TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
@@ -322,7 +322,7 @@ namespace TRE
 			ECSSystemManager::Instance().GetSystem<ParentingSystem>()->SetParent(child8, parent4);
 		}
 
-		SceneManager::Instance().SaveSceneAs("../Scenes/DemoScene.json");
+		//SceneManager::Instance().SaveSceneAs("../Scenes/DemoScene.json");
 	}
 }
 #pragma endregion TO DELETE TEST
