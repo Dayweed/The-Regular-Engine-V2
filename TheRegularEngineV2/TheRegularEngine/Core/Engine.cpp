13671f06da36bad69dcc2ff66c3fe9a607010a0c
Toby IO <toby-io@hotmail.com>
2023-10-23 13:28:49 +0800

    There was a glitch in the matrix

--- TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
@@ -323,7 +323,7 @@ namespace TRE
 			ECSSystemManager::Instance().GetSystem<ParentingSystem>()->SetParent(child8, parent4);
 		}
 
-		SceneManager::Instance().SaveSceneAs("../Scenes/DemoScene.json");
+		//SceneManager::Instance().SaveSceneAs("../Scenes/DemoScene.json");
 	}
 }
 #pragma endregion TO DELETE TEST
@@ -459,6 +459,8 @@ namespace TRE
 				Profiler::Instance().StartTimer("OnReset");
 				ECSSystemManager::Instance().OnReset();
 				Profiler::Instance().EndTimer("OnReset");
+
+				GameLoop::Instance().SetSceneReset(false);
 			}
 
 			Profiler::Instance().StartTimer("OnDestroyEntities");
