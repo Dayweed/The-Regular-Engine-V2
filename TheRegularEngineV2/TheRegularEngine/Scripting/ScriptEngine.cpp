4f58dce7d5840fc5d5dbdb4787a77568de3c3f06
Toby IO <toby-io@hotmail.com>
2023-10-31 01:34:35 +0800

    Hold

--- TheRegularEngineV2/TheRegularEngine/Scripting/ScriptEngine.cpp
+++ TheRegularEngineV2/TheRegularEngine/Scripting/ScriptEngine.cpp
@@ -305,6 +305,66 @@ namespace TRE
 		InitScriptingMain();
 	}
 
+	void ScriptEngine::CreateCSEntityData(Entity entity)
+	{
+		const auto& scriptComponent = entity->GetComponent<ScriptComponent>();
+		if (EntityClassExists(scriptComponent.m_StoredClass))
+		{
+			std::string GUID = entity->GetGUID();
+
+			if (s_ScriptEngineData->ScriptInstances.find(GUID) != s_ScriptEngineData->ScriptInstances.end()) return;
+
+			std::shared_ptr<ScriptInstance> instance = std::make_shared<ScriptInstance>(s_ScriptEngineData->ScriptClasses[scriptComponent.m_StoredClass], GUID);
+			s_ScriptEngineData->ScriptInstances[GUID] = instance;
+
+			if (s_ScriptEngineData->EntityFieldMap.find(GUID) != s_ScriptEngineData->EntityFieldMap.end())
+			{
+				ScriptFieldMap& fieldMap = s_ScriptEngineData->EntityFieldMap[GUID];
+				for (auto& field : fieldMap)
+				{
+					instance->GetInternalFieldValue(field.first, field.second.m_buffer);
+				}
+			}
+		}
+	}
+	void ScriptEngine::GetCSEntityData(Entity entity)
+	{
+		const auto& scriptComponent = entity->GetComponent<ScriptComponent>();
+		if (EntityClassExists(scriptComponent.m_StoredClass))
+		{
+			std::string GUID = entity->GetGUID();
+
+			if (s_ScriptEngineData->ScriptInstances.find(GUID) == s_ScriptEngineData->ScriptInstances.end()) CreateCSEntityData(entity);
+
+			std::shared_ptr<ScriptInstance> instance = s_ScriptEngineData->ScriptInstances[GUID];
+
+			ScriptFieldMap& fieldMap = s_ScriptEngineData->EntityFieldMap[GUID];
+			for (auto& field : fieldMap)
+			{
+				instance->GetInternalFieldValue(field.first, field.second.m_buffer);
+			}
+		}
+	}
+
+	void ScriptEngine::UpdateCSEntityData(Entity entity)
+	{
+		const auto& scriptComponent = entity->GetComponent<ScriptComponent>();
+		if (EntityClassExists(scriptComponent.m_StoredClass))
+		{
+			std::string GUID = entity->GetGUID();
+
+			if (s_ScriptEngineData->ScriptInstances.find(GUID) == s_ScriptEngineData->ScriptInstances.end()) CreateCSEntityData(entity);
+
+			std::shared_ptr<ScriptInstance> instance = s_ScriptEngineData->ScriptInstances[GUID];
+
+			ScriptFieldMap& fieldMap = s_ScriptEngineData->EntityFieldMap[GUID];
+			for (auto& field : fieldMap)
+			{
+				instance->SetInternalFieldValue(field.first, field.second.m_buffer);
+			}
+		}
+	}
+
 	void ScriptEngine::InitScriptingMain()
 	{
 		MonoImage* assemblyImage = mono_assembly_get_image(s_ScriptEngineData->MonoAssembly);
@@ -386,18 +446,9 @@ namespace TRE
 		{
 			std::string GUID = entity->GetGUID();
 
-			std::shared_ptr<ScriptInstance> instance = std::make_shared<ScriptInstance>(s_ScriptEngineData->ScriptClasses[scriptComponent.m_StoredClass], GUID);
-			s_ScriptEngineData->ScriptInstances[GUID] = instance;
+			if (s_ScriptEngineData->ScriptInstances.find(GUID) == s_ScriptEngineData->ScriptInstances.end()) UpdateCSEntityData(entity);
 
-			if(s_ScriptEngineData->EntityFieldMap.find(GUID) != s_ScriptEngineData->EntityFieldMap.end())
-			{
-				ScriptFieldMap& fieldMap = s_ScriptEngineData->EntityFieldMap[GUID];
-				for(auto& field : fieldMap)
-				{
-					instance->SetInternalFieldValue(field.first, field.second.m_buffer);
-				}
-			}
-
+			std::shared_ptr<ScriptInstance> instance = s_ScriptEngineData->ScriptInstances[GUID];
 			instance->OnCreateInvoke();
 		}
 	}
