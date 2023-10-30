80af10062337314fb088d202449c14199a0e8616
Toby IO <toby-io@hotmail.com>
2023-10-31 03:18:48 +0800

    How? Why? Damn Thicc contenedores...

--- TheRegularEngineV2/TheRegularEngine/Core/ECS.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/ECS.cpp
@@ -365,7 +365,8 @@ namespace TRE
 		// First element of each array keeps the amount of elements. 
 		if (m_Current.empty()) {
 			m_Current = nlohmann::json::array();
-			m_Current.push_back(ECSManager::Instance().GetAllEntities().size());
+			m_Current.push_back(1);
+			//m_Current.push_back(ECSManager::Instance().GetAllEntities().size()); 	// This somehows kills the entt if too fat
 		}
 		else
 		{
