8da918caafdb1d0969b41251985a8b945d7dbff2
zhengrongg <zhengrong29@gmail.com>
2023-10-31 13:52:34 +0800

    Multiple key input in C#

--- TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
+++ TheRegularEngineV2/TheRegularEngine/Core/Engine.cpp
@@ -256,34 +256,6 @@ namespace TRE
 			light->GetComponent<Transform>().m_IsDirty = true;
 		}
 
-		//{
-		//	// Parent child prefabing test
-		//	Entity prefabParent = ECSManager::Instance().CreateEntity();
-		//	prefabParent->GetComponent<Properties>().m_Name = "prefabParent";
-		//	Transform& transform3 = prefabParent->GetComponent<Transform>();
-		//	transform3.m_Position = glm::vec3(0.f, 50.f, 100.f);
-		//	transform3.m_Scale = glm::vec3(0.2f, 0.2f, 0.2f);
-		//	transform3.m_Rotation = glm::vec3(0, 180.f, 0);
-		//	transform3.m_IsDirty = true;
-		//	prefabParent->AddComponent<MeshRenderer>();
-		//	meshRendererSystem->SetMeshRenderer(prefabParent, ResourceManager::Instance().GetResource<RenderObject>(skullHandle));
-		//	meshRendererSystem->SetMaterial(prefabParent, ResourceManager::Instance().GetResource<Material>(matHandle));
-
-		//	Entity prefabChild = ECSManager::Instance().CreateEntity();
-		//	prefabChild->GetComponent<Properties>().m_Name = "prefabChild";
-		//	Transform& transform4 = prefabChild->GetComponent<Transform>();
-		//	transform4.m_Position = glm::vec3(-100.f, 50.f, 100.f);
-		//	transform4.m_Scale = glm::vec3(0.1f, 0.1f, 0.1f);
-		//	transform4.m_Rotation = glm::vec3(0, 180.f, 0);
-		//	transform4.m_IsDirty = true;
-		//	prefabChild->AddComponent<MeshRenderer>();
-		//	prefabChild->AddComponent<FAKEFEL>();
-		//	meshRendererSystem->SetMeshRenderer(prefabChild, ResourceManager::Instance().GetResource<RenderObject>(skullHandle));
-		//	meshRendererSystem->SetMaterial(prefabChild, ResourceManager::Instance().GetResource<Material>(matHandle));
-
-		//	ECSSystemManager::Instance().GetSystem<ParentingSystem>()->AddChild(prefabParent, prefabChild);
-		//}
-
 		{
 			//dont delete this
 			//testing hierarchy entities
--- TheRegularEngineV2/TheRegularEngine/InputHandler/InputHandler.h
+++ TheRegularEngineV2/TheRegularEngine/InputHandler/InputHandler.h
@@ -3,68 +3,70 @@
 struct GLFWwindow;
 namespace TRE
 {
-    /*!*****************************************************************************
-    *\brief 	Contains static functions for glfw's callback functions.
-    *
-    *******************************************************************************/
-    class InputHandler
-    {
-        public:
-        enum class MouseCode
-        {
-            LEFTCLICK, RIGHTCLICK, MIDDLE, BOTTOMBUTTON, TOPBUTTON
-        };
-        /*!*****************************************************************************
-        *\brief     Response function for keyboard callbacks.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	key         key token.
-        *\param 	scancode    key scancode.
-        *\param 	action      key action. (Pressed/Held/Released..)
-        *\param 	mod         key mods. (Shift/Capslock/Ctrl..)
-        *******************************************************************************/
-        static void KeyCb(GLFWwindow* win_ptr, int key, int scancode, int action, int mod);
-        /*!*****************************************************************************
-        *\brief 	Response function for mouse button callbacks.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	button      mouse button.
-        *\param 	action      key action. (Pressed/Held/Released..)
-        *\param 	mod         key mods. (Shift/Capslock/Ctrl..)
-        *******************************************************************************/
-        static void MouseButtonCb(GLFWwindow* win_ptr, int button, int action, int mod);
-        /*!*****************************************************************************
-        *\brief 	Response function for mouse scroll callbacks.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	xoffset     new xoffset of scrollwheel.
-        *\param 	yoffset     new yoffset of scrollwheel.
-        *******************************************************************************/
-        static void MouseScrollCb(GLFWwindow* win_ptr, double xoffset, double yoffset);
-        /*!*****************************************************************************
-        *\brief 	Response function for mouse position callbacks.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	xpos        new mouse position's x value.
-        *\param 	ypos        new mouse position's y value.
-        *******************************************************************************/
-        static void MousePosCb(GLFWwindow* win_ptr, double xpos, double ypos);
-        /*!*****************************************************************************
-        *\brief 	Response function for when mouse is focus on application window.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	entered     new window focus state.
-        *******************************************************************************/
-        static void MouseFocusCb(GLFWwindow* win_ptr, int entered);
-        /*!*****************************************************************************
-        *\brief 	Response function for when mouse is button is held on application
-        window.
-        *
-        *\param 	win_ptr     pointer to GLFW window.
-        *\param 	entered     new window focus state.
-        *******************************************************************************/
-        static void CheckMouseEvent(GLFWwindow* win_ptr, int button, int action);
+	/*!*****************************************************************************
+	*\brief 	Contains static functions for glfw's callback functions.
+	*
+	*******************************************************************************/
+	class InputHandler
+	{
+	public:
+		enum class MouseCode
+		{
+			LEFTCLICK, RIGHTCLICK, MIDDLE, BOTTOMBUTTON, TOPBUTTON
+		};
+		/*!*****************************************************************************
+		*\brief     Response function for keyboard callbacks.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	key         key token.
+		*\param 	scancode    key scancode.
+		*\param 	action      key action. (Pressed/Held/Released..)
+		*\param 	mod         key mods. (Shift/Capslock/Ctrl..)
+		*******************************************************************************/
+		static void KeyCb(GLFWwindow* win_ptr, int key, int scancode, int action, int mod);
+		/*!*****************************************************************************
+		*\brief 	Response function for mouse button callbacks.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	button      mouse button.
+		*\param 	action      key action. (Pressed/Held/Released..)
+		*\param 	mod         key mods. (Shift/Capslock/Ctrl..)
+		*******************************************************************************/
+		static void MouseButtonCb(GLFWwindow* win_ptr, int button, int action, int mod);
+		/*!*****************************************************************************
+		*\brief 	Response function for mouse scroll callbacks.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	xoffset     new xoffset of scrollwheel.
+		*\param 	yoffset     new yoffset of scrollwheel.
+		*******************************************************************************/
+		static void MouseScrollCb(GLFWwindow* win_ptr, double xoffset, double yoffset);
+		/*!*****************************************************************************
+		*\brief 	Response function for mouse position callbacks.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	xpos        new mouse position's x value.
+		*\param 	ypos        new mouse position's y value.
+		*******************************************************************************/
+		static void MousePosCb(GLFWwindow* win_ptr, double xpos, double ypos);
+		/*!*****************************************************************************
+		*\brief 	Response function for when mouse is focus on application window.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	entered     new window focus state.
+		*******************************************************************************/
+		static void MouseFocusCb(GLFWwindow* win_ptr, int entered);
+		/*!*****************************************************************************
+		*\brief 	Response function for when mouse is button is held on application
+		window.
+		*
+		*\param 	win_ptr     pointer to GLFW window.
+		*\param 	entered     new window focus state.
+		*******************************************************************************/
+		static void CheckMouseEvent(GLFWwindow* win_ptr, int button, int action);
 
-        private:
-    };
+		static bool GetKeyState(int key);
+	private:
+		static std::unordered_map<int, int> m_keyMap;
+	};
 }
