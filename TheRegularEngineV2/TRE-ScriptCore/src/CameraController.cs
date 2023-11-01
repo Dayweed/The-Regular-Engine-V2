6837e2457b72acb008339ef1612ab6766c93b981
zhengrongg <zhengrong29@gmail.com>
2023-11-01 17:12:20 +0800

    game tweaking

--- TheRegularEngineV2/TRE-ScriptCore/src/CameraController.cs
+++ TheRegularEngineV2/TRE-ScriptCore/src/CameraController.cs
@@ -7,6 +7,7 @@ namespace TRE
 	public class CameraController : Entity
 	{
 		private Entity Player1;
+		private Entity Player2;
 
 		private MoleController moleController;
 		private float distance = 35;
@@ -17,7 +18,8 @@ namespace TRE
 		private float distanceD = 50;
 		private float distanceE = 50;
 		private float distanceF = 65;
-		private float distanceG = 40;
+		private float distanceG = 50;
+		private float distanceH = 20;
 
 		private float lerpTime = 0.01f;
 
@@ -25,11 +27,13 @@ namespace TRE
 		{
 			Player1 = ECSManager.FindEntityByName("Holey");
 			moleController = Player1.GetComponent<MoleController>();
+			Player2 = ECSManager.FindEntityByName("Moley");
 		}
 
 		public void Update()
 		{
-			TransformSystem.GetPosition(Player1.ID, out Vector3 pos);
+			TransformSystem.GetPosition(Player1.ID, out Vector3 holeyPos);
+			TransformSystem.GetPosition(Player2.ID, out Vector3 moleyPos);
 
 			if(moleController.regionA)
 			{
@@ -69,10 +73,19 @@ namespace TRE
 
 			if (moleController.regionG)
 			{
-				CameraSystem.TransitionMainCamera(new Vector3(0, 40, 50), new Vector3(15, 180, 0), 0.001f);
+				CameraSystem.TransitionMainCamera(new Vector3(50, 40, 50), new Vector3(35, 200, 0), 0.001f);
 				distance = MathF.Lerp(distance, distanceG, lerpTime);
 			}
 
+			if (moleController.regionH) 
+			{
+				CameraSystem.TransitionMainCamera(new Vector3(50, 40, 50), new Vector3(20, 180, 0), 0.001f);
+				distance = MathF.Lerp(distance, distanceH, lerpTime);
+
+			}
+
+			Vector3 pos = moleyPos + holeyPos;
+			pos /= 2;
 			CameraSystem.SetMainCameraLookAt(pos, distance);
 		}
 	}
