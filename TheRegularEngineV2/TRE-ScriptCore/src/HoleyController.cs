bcd710b969e2bcd8deb3c47de11cb9c5cb19db9f
Toby IO <toby-io@hotmail.com>
2023-11-05 17:22:01 +0800

    Bug Fix: Major visible movement when Moley scales Fixed roation on holey and moley

--- TheRegularEngineV2/TRE-ScriptCore/src/HoleyController.cs
+++ TheRegularEngineV2/TRE-ScriptCore/src/HoleyController.cs
@@ -81,9 +81,10 @@ namespace TRE
 		{
 			// Move The Test Object 
 			TransformSystem.GetPosition(this.ID, out Vector3 pos);
+            TransformSystem.SetRotation(this.ID, new Vector3(0, 0, 0));
 
-			//Movement Related stuff
-			PS.GetLinearVelocity(this.ID, out Vector3 currVelocity);
+            //Movement Related stuff
+            PS.GetLinearVelocity(this.ID, out Vector3 currVelocity);
 
 			dirVec = new Vector3(0, 0, 0);
 			#region Movement
@@ -283,10 +284,10 @@ namespace TRE
 				}
 			}
 
-			TransformSystem.SetRotation(this.ID, playerDirection);
+            TransformSystem.SetRotation(this.ID, new Vector3(0, playerDirection.y, 0));
 
 
-			if (Key.ID != 0 && FinalPlatform.ID != 0)
+            if (Key.ID != 0 && FinalPlatform.ID != 0)
 			{
 				if (PS.IsTriggerEnter(this.ID, Key.ID))
 				{
