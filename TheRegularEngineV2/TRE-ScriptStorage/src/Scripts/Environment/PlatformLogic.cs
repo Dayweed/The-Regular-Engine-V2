14b2513326825ce8ef7348380da87f85797d6643
Toby IO <toby-io@hotmail.com>
2023-11-13 16:52:18 +0800

    Mild change to level 1

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/Environment/PlatformLogic.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/Environment/PlatformLogic.cs
@@ -27,7 +27,7 @@ namespace TRE
             if (Scene.GetSceneName() == "Level_1" && name == "Platform_1")
             {
                 currentIndex = 0;
-                positions = new vec3[] { new vec3(10, 18, -210), new vec3(10, 18, -174) };
+                positions = new vec3[] { new vec3(22, 18, -210), new vec3(22, 18, -174) };
             }
         }
 
