b8ca71737f1f62538a6d11a83fbc20c81bf711d3
zhengrongg <90662203+xuanxuanlee@users.noreply.github.com>
2023-11-14 00:19:16 +0800

    update audiosystem 2

--- TheRegularEngineV2/TheRegularEngine/Audio/AudioSystem.h
+++ TheRegularEngineV2/TheRegularEngine/Audio/AudioSystem.h
@@ -30,6 +30,8 @@ namespace TRE
 		float m_MaxDistance{ 300.f };
 
 		bool m_HasCompiled{ false };
+		bool m_Played{ false };
+
 		FMOD_VECTOR m_goPosition{ 0.0f, 0.0f, 0.0f };
 		property_vtable()
 			//std::map<int, std::string> channelIndex{};
@@ -201,6 +203,8 @@ namespace TRE
 		;
 		std::unordered_set<Entity> audioMap;
 		std::unordered_map<Entity, FMOD::Sound*> soundToRemove;
+
+		
 
 	};
 
