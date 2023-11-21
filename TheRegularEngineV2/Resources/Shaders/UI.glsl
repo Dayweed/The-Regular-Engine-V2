28e15e75f10c203bc9c2054e3af6fa0a71f3f8ab
Pancakes <jerelpeck99@gmail.com>
2023-11-21 19:11:20 +0800

    Pull Mrs Tan

--- TheRegularEngineV2/Resources/Shaders/UI.glsl
+++ TheRegularEngineV2/Resources/Shaders/UI.glsl
@@ -47,4 +47,6 @@ layout (set = 0, binding = 1) uniform sampler2D UI_Texture;
 void main()
 {
     outColor = texture(UI_Texture, In.UV) * In.Color;
+    outColor.rgb = pow(outColor.rgb, vec3(1.0 / 2.2));
+    outColor.w = In.Color.w;
 }
