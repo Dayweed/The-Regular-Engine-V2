446ba08cdfaec5412e1f1d03aeeeaef390928c2d
Pancakes <jerelpeck99@gmail.com>
2023-11-21 22:06:56 +0800

    SceneTransitionMainMenu

---
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/MainMenuSceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+using System.Threading;
+
+namespace TRE
+{
+    public class MainMenuSceneTransition : Entity
+    {
+        private string TutorialSceneName;
+        private string ControlsDisplaySceneName;
+
+        public void Start()
+        {
+            TutorialSceneName = "Tutorial";
+            ControlsDisplaySceneName = "ControlsDisplay";
+        }
+
+        public void Update()
+        {
+            if (InputSystem.GetKeyTrigger(InputKeys.Space))
+            {
+                Scene.ChangeScene(TutorialSceneName);
+            }
+
+            //if (InputSystem.GetKeyTrigger(InputKeys.Space))
+            //{
+            //    Scene.ChangeScene(ControlsDisplaySceneName);
+            //}
+        }
+    }
+}
