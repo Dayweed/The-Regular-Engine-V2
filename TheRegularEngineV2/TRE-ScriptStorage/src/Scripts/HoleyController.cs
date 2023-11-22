bea25a44db6a1ceba986b912ece7787f00223119
zhengrongg <zhengrong29@gmail.com>
2023-11-23 00:13:44 +0800

    tut backup

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/HoleyController.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/HoleyController.cs
@@ -374,8 +374,32 @@ namespace TRE
 			#endregion
 
 			dirVec.y = 0;
-			if (dirVec != new vec3())
+			if (dirVec != vec3.Zero)
+			{
 				dirVec = dirVec.Normalized;
+
+				//Walking animation
+				//if (GetComponent<MeshRenderer>().Mesh != "38f15a6260750456")
+				//{
+				//	GetComponent<MeshRenderer>().Mesh = "38f15a6260750456";
+				//	//if (HasComponent<Animation>() == false)
+				//	//{
+				//	//	AddComponent<Animation>();
+				//	//}
+				//}	
+			}
+			else
+			{
+				//Idle animation
+				//if (GetComponent<MeshRenderer>().Mesh != "6ee6fad4e6ecaab8")
+				//{
+				//	GetComponent<MeshRenderer>().Mesh = "6ee6fad4e6ecaab8";
+				//	//if (HasComponent<Animation>() == false)
+				//	//{
+				//	//	AddComponent<Animation>();
+				//	//}
+				//}
+			}
 
 			playerDirection = lastPlayerDirection + (int)CS.GetMainCameraRotation().y;
 			playerDirection = (playerDirection % 360);
