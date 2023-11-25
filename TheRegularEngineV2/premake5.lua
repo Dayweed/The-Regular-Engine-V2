1986552848a90470b4c7eb188298da59dc165c82
zhengrongg <zhengrong29@gmail.com>
2023-11-25 14:53:46 +0800

    try

--- TheRegularEngineV2/premake5.lua
+++ TheRegularEngineV2/premake5.lua
@@ -371,8 +371,8 @@ project "TRE-ScriptCore"
 	language "C#"
 	dotnetframework "4.7.2"
 
-	targetdir ("Executable_" .. outputdir .. "/")
-	objdir ("Executable_" .. outputdir .. "/")
+	targetdir ("Resources/Scripts")
+	objdir ("Resources/Scripts")
 	
 	libdirs"Resources/Scripts"
 	links"Coroutine.dll"
@@ -398,8 +398,8 @@ project "TRE-ScriptStorage"
 	dotnetframework "4.7.2"
 
 	-- (DON'T DELETE THIS!)
-	targetdir ("Executable_" .. outputdir .. "/")
-	objdir ("Executable_" .. outputdir .. "/")
+	targetdir ("Resources/Scripts")
+	objdir ("Resources/Scripts")
 	
 	libdirs "Resources/Scripts"
 	links "TRE-ScriptCore.dll"
