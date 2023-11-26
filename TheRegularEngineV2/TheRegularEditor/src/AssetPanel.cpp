4824839de4cd77c3066b5ef2bae91c6142f07447
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2023-11-26 11:30:09 +0800

    displayed text in asset panel is now copyable!

--- TheRegularEngineV2/TheRegularEditor/src/AssetPanel.cpp
+++ TheRegularEngineV2/TheRegularEditor/src/AssetPanel.cpp
@@ -23,14 +23,23 @@ namespace TRE
 			ImGui::Begin("Asset Panel", &m_ShowAssetPanel);
 			for (const auto& asset : AssetManager::Instance().GetAssets())
 			{
-				constexpr unsigned maxAssetNameSize = 25;
+				constexpr unsigned maxAssetNameSize = 35;
 				std::string assetName(asset.first);
 
 				if (assetName.size() < maxAssetNameSize)
 					assetName.append(std::string(maxAssetNameSize - assetName.size(), ' '));
 
+				// should the GUID be padded somehow to make sure it's 16 characters long for viewing consistency?
+				// some GUIDs aren't 16 chars when displayed...
 				std::string print = assetName + " | " + Resource::GetGUIDHex(asset.second.first) + " | " + (asset.second.second ? "Compiled" : "Not Compiled");
-				ImGui::Text(print.c_str());
+
+				// make the displayed text highlightable/selectable
+				// from https://github.com/ocornut/imgui/issues/950#issuecomment-840541529
+				ImGui::PushID(print.c_str());
+				ImGui::PushItemWidth(ImGui::GetWindowSize().x);
+				ImGui::InputText("", (char*)print.c_str(), print.size(), ImGuiInputTextFlags_ReadOnly);
+				ImGui::PopItemWidth();
+				ImGui::PopID();
 				ImGui::Spacing();
 			}
 			ImGui::End();
