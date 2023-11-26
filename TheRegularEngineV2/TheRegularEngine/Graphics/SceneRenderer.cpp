f9e6418c636c0bd39e57d644ccd4c6a5bcdf4732
Pancakes <jerelpeck99@gmail.com>
2023-11-26 17:40:47 +0800

    Prevent speed = 0 infinite loop

--- TheRegularEngineV2/TheRegularEngine/Graphics/SceneRenderer.cpp
+++ TheRegularEngineV2/TheRegularEngine/Graphics/SceneRenderer.cpp
@@ -381,7 +381,10 @@ namespace TRE
 				continue;
 
 			if (AnimComp.m_IsAnimating)
+			{
+				AnimComp.m_AnimationSpeed <= 0 ? AnimComp.m_AnimationSpeed = 0.01f : AnimComp.m_AnimationSpeed;
 				MRComp.m_RenderObject->UpdateAnimation(AnimComp.m_BufferData.L2W, TransformComp.m_WorldXform, AnimComp.m_FPS, AnimComp.m_AnimationSpeed);
+			}
 
 			AnimComp.m_UBO->SetData(&AnimComp.m_BufferData, sizeof(AnimationUBO));
 		}
