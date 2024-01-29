7ff6a8eb32ca4df38ba3a9568482ab4d1ef3c177
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2024-01-29 15:29:24 +0800

    simple QOL when selecting scripts 👍🏼

--- TheRegularEngineV2/TheRegularEditor/src/InspectorPanel.cpp
+++ TheRegularEngineV2/TheRegularEditor/src/InspectorPanel.cpp
@@ -643,6 +643,7 @@ namespace TRE
 							if(ImGui::BeginCombo("##Scripts", "Scripts"))
 							{
 								std::vector<std::string> scripts = ScriptEngine::s_ScriptEngineData->RegisteredScriptClasses;
+								std::sort(scripts.begin(), scripts.end());
 								for (auto& script : scripts)
 								{
 									if (ImGui::Selectable(script.c_str()))
