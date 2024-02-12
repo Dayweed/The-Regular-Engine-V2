13f50cda4c3ff68e403775b3ebd91cf02e90ca05
Toby IO <toby-io@hotmail.com>
2024-02-12 17:49:17 +0800

    Fix Bug: VFX_FadeIn and VFX_FadeOut does not account for child not having sprite renderer

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/Effects/VFX_FadeOut.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/Effects/VFX_FadeOut.cs
@@ -9,11 +9,11 @@ using GlmSharp;
 namespace TRE
 {
 	public class VFX_FadeOut : Entity
-    {
-        vec4 OriginalColor;
+	{
+		vec4 OriginalColor;
 
 		bool fading = false;
-        bool doneFading = false;
+		bool doneFading = false;
 
 		float fadingSpeed = 1f;
 
@@ -22,70 +22,79 @@ namespace TRE
 		public void Start()
 		{
 
-        }
+		}
 
 		public void Update()
 		{
-            if (!fading) return;
+			if (!fading) return;
 
-            if (HasComponent<SpriteRenderer>())
-            {
-                //float w = MathF.Lerp(MyRenderer.Color.w, 0, fadingSpeed * Time.deltaTime);
-                float w = MyRenderer.Color.w - fadingSpeed * Time.deltaTime;
-                MyRenderer.Color = new vec4(OriginalColor.x, OriginalColor.y, OriginalColor.z, w);
+			if (HasComponent<SpriteRenderer>())
+			{
+				//float w = MathF.Lerp(MyRenderer.Color.w, 0, fadingSpeed * Time.deltaTime);
+				float w = MyRenderer.Color.w - fadingSpeed * Time.deltaTime;
+				MyRenderer.Color = new vec4(OriginalColor.x, OriginalColor.y, OriginalColor.z, w);
 
-                // Make sure all its children also fade
-                UpdateChildren(this, w);
+				// Make sure all its children also fade
+				UpdateChildren(this, w);
 
-                if (MyRenderer.Color.w <= 0)
-                {
-                    ForceComplete();
-                }
-            }
-        }
+				if (MyRenderer.Color.w <= 0)
+				{
+					ForceComplete();
+				}
+			}
+		}
 
-        private void UpdateChildren(Entity entity, float alpha)
-        {
-            for (int i = 0; i < entity.parenting.GetTotalChildren(); ++i)
-            {
-                Entity child = entity.parenting.GetChild(i);
-                if (HasComponent<SpriteRenderer>())
-                {
-                    SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
-                    spriteRenderer.Color = new vec4(spriteRenderer.Color.x, spriteRenderer.Color.y, spriteRenderer.Color.z, alpha);
-                }
-                UpdateChildren(child, alpha);
-            }
-        }
-
-        public void FadeOut()
+		private void UpdateChildren(Entity entity, float alpha)
 		{
-            if (fading) return;
+			for (int i = 0; i < entity.parenting.GetTotalChildren(); ++i)
+			{
+				Entity child = entity.parenting.GetChild(i);
+				if (child.HasComponent<SpriteRenderer>())
+				{
+					SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+					spriteRenderer.Color = new vec4(spriteRenderer.Color.x, spriteRenderer.Color.y, spriteRenderer.Color.z, alpha);
+				}
+				else if (child.HasComponent<Text>())
+				{
+					child.GetComponent<Text>().IsVisible = false;
+                }
+				UpdateChildren(child, alpha);
+			}
+		}
 
-            MyRenderer = GetComponent<SpriteRenderer>();
-            OriginalColor = MyRenderer.Color;
+		public void FadeOut()
+		{
+			if (fading) return;
 
-            MyRenderer.isVisible = true;
-            fading = true;
-            doneFading = false;
-        }
+			if (HasComponent<SpriteRenderer>())
+			{
+				MyRenderer = GetComponent<SpriteRenderer>();
+
+				OriginalColor = MyRenderer.Color;
+
+				MyRenderer.isVisible = true;
+
+                fading = true;
+                doneFading = false;
+            }
+		}
 
 		public bool DoneFading()
 		{
 			return doneFading;
-        }
+		}
 
-        public void ForceComplete()
-        {
-            MyRenderer.Color = new vec4(OriginalColor.x, OriginalColor.y, OriginalColor.z, 0);
-            UpdateChildren(this, 0);
-            fading = false;
-            doneFading = true;
-        }
+		public void ForceComplete()
+		{
+			MyRenderer.Color = new vec4(OriginalColor.x, OriginalColor.y, OriginalColor.z, 0);
+			UpdateChildren(this, 0);
+			fading = false;
+			doneFading = true;
+		}
 
-        public void ForceStop()
-        {
-            fading = false;
-        }
-    }
+		public void ForceStop()
+		{
+			fading = false;
+		}
+	}
 }
