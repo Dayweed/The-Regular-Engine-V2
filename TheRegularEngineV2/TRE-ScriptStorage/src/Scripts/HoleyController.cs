c9b6b1c17cc224b37aa2834c4c24ce79b21bf083
PocketsPocket <royceleewh@gmail.com>
2024-02-28 22:39:09 +0800

    when they die the camera stops moving

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/HoleyController.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/HoleyController.cs
@@ -346,6 +346,14 @@ namespace TRE
 		{
 			if (playerPosition.y < OutofMapPos.y)
 			{
+				PS.GetLinearVelocity(this.ID, out vec3 currVelocity);
+				if (currVelocity.x > 0 || currVelocity.z > 0)
+				{
+					currVelocity.x = 0;
+					currVelocity.z = 0;
+					PS.SetLinearVelocity(this.ID, currVelocity);
+				}
+
 				isDead = true;
 				DroppingOutOfMap = true;
 
