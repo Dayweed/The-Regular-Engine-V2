ac45b2c57bb321d311307eb325c1e7fc21f48987
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2024-03-06 14:58:26 +0800

    1x OnOff platform but better i think
    + added trembling function, to show players that platform is gonna rotate

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/OnOffPlatform.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/OnOffPlatform.cs
@@ -5,44 +5,122 @@ namespace TRE
 {
 	using TS = TransformSystem;
 
-	class OnOffPlatform : Entity
+	public class OnOffPlatform : Entity
 	{
+		#region Constants (assigned in Start(), no changes please thanks)
+		// an array of cardinal directions from 45 to 315
+		int[] directions = new int[7];
+
+		// the angle the platform must have to be in the ACTIVE position
+		float platformActiveAngle;
+
+		// the angle the platform must have to be in the INACTIVE position
+		float platformInactiveAngle;
+
+		// the direction (1 or -1) to rotate in when activating the platform
+		int activationRotationDirection;
+		#endregion
+
+		// whether the platform is currently active/inactive
 		bool platformState;
+
+		// setting this to true will begin the rotation
 		bool shouldRotate;
 
-		float timer;
-		const float defaultDuration = 3.0f;
-		float duration;
-		float rotateStartTime;
-		float rotateStart;
-		float rotateEnd;
+		// the angle at the beginning of the rotation
+		float rotateStartAngle;
 
-		// for convenience's sake
-		Action<string> print = Console.WriteLine;
+		// the angle to have at the end of the rotation
+		float rotateEndAngle;
+
+		// determines which way to rotate, value is either 1 or -1
+		int rotationDirection;
+
+		// the change in angle per unit(frame?) of time in a rotation
+		const float rotationSpeed = 40.0f;
+
+		float timer;
+		vec3 originalPosition;
 
 		public void Start()
 		{
-			platformState = false;
-			shouldRotate = false;
+			// I can't do just the line below... :(
+			// int[] directions = { 45, 90, 135, 180, 225, 270, 315 };
+			for (int i = 0; i < 7; ++i)
+				directions[i] = 45 * (i + 1);
 
-			timer = 0.0f;
-			duration = defaultDuration;
-			rotateStartTime = 0.0f;
-			rotateStart = -90.0f;
-			rotateEnd = 0.0f;
+			TS.GetRotation(this.ID, out vec3 initialRot);
+			float initialRotY = NiceRotationAngle(initialRot.y);
+
+			// set constants based on initialRotY
+			if (initialRotY == 0)
+			{
+				platformInactiveAngle = 0.0f;
+				platformActiveAngle = 90.0f;
+				activationRotationDirection = 1;
+				// use the z axis to rotate
+			}
+			else if (initialRotY == 90)
+			{
+				// FUCKING GIMBAL LOCK?!?
+			}
+			else if (initialRotY == 180)
+			{
+				platformInactiveAngle = 0.0f;
+				platformActiveAngle = -90.0f;
+				activationRotationDirection = -1;
+				// use the z axis to rotate
+			}
+			else if (initialRotY == 270)
+			{
+				// FUCKING GIMBAL LOCK?!?
+			}
+
+			platformState = false;
+			timer = 0;
+			TS.GetPosition(this.ID, out vec3 pos);
+			originalPosition = pos;
 		}
 
 		public void Update()
 		{
-			if (InputSystem.GetKeyPress(InputKeys.KPAdd))
-				SetPlatformState(true);
-			if (InputSystem.GetKeyPress(InputKeys.KPEnter))
+			if (InputSystem.GetKeyPress(InputKeys.LeftBracket))
 				SetPlatformState(false);
+			if (InputSystem.GetKeyPress(InputKeys.RightBracket))
+				SetPlatformState(true);
 
 			timer += Time.deltaTime;
 
+			// TrembleZ();
+
 			if (shouldRotate)
-				PerformRotation();
+				PerformRotationZ();
+		}
+
+		private bool IsInRange(float angle, float direction)
+		{
+			const float rangeInDegrees = 5.0f;
+			if (direction - rangeInDegrees <= angle && angle <= direction + rangeInDegrees)
+				return true;
+			else
+				return false;
+		}
+
+		// a 'nice' rotation is a rotation in degrees of only the following:
+		// { 0, 45, 90, 135, 180, 225, 270, 315 }
+		private float NiceRotationAngle(float rotYInDegrees)
+		{
+			while (rotYInDegrees < 0)
+				rotYInDegrees += 360;
+			while (rotYInDegrees > 360)
+				rotYInDegrees -= 360;
+
+			for (int i = 0; i < directions.Length; ++i)
+			{
+				if (IsInRange(rotYInDegrees, directions[i]))
+					return directions[i];
+			}
+			return 0;
 		}
 
 		public bool GetPlatformState()
@@ -53,91 +131,78 @@ namespace TRE
 		public void SetPlatformState(bool state)
 		{
 			// do nothing if the platform already matches the given state
-			if (platformState == state)
+			if (!shouldRotate && platformState == state)
 			{
-				print("already there bro. :)");
 				return;
 			}
 
-			// continue as normal if incoming state is the one being worked on
-			// this is checked by seeing if the platform has reached its goal and what its goal is
-			bool workingOnActive = platformState != state && rotateEnd == 0;
-			bool workingOnInactive = platformState != state && rotateEnd == -90.0f;
-			if (shouldRotate && (workingOnActive || workingOnInactive))
+			// send the "signal" that a rotation should take place
+			shouldRotate = true;
+
+			// assign variables based on the constants set in Start()
+			if (state)
 			{
-				print("bro i'm working on that already");
-				print("workingOnActive: " + workingOnActive.ToString());
-				print("workingOnInactive: " + workingOnInactive.ToString());
-				print("");
-				return;
+				// go from inactive to active
+				rotateStartAngle = platformInactiveAngle;
+				rotateEndAngle = platformActiveAngle;
+
+				rotationDirection = activationRotationDirection;
 			}
-
-			// let the rotation know what time it began
-			rotateStartTime = timer;
-
-			// if it was ALREADY rotating beforehand, start from its current rotation
-			if (shouldRotate)
+			else
 			{
-				TS.GetRotation(this.ID, out vec3 currRot);
-				float currRotAngle = currRot.z;
-				const float defaultRotSpeed = 90 / defaultDuration;
+				// go from active to inactive
+				rotateStartAngle = platformActiveAngle;
+				rotateEndAngle = platformInactiveAngle;
 
-				if (!state) // <- should that just be `state`...hmm...
-				{
-					print("trying to go UP from middle of rotation");
-					// start from current, end at 0
-					rotateStart = currRotAngle;
-					rotateEnd = 0.0f;
-					// should be the same speed as normal
-					duration = Math.Abs(rotateEnd - rotateStart) / defaultRotSpeed;
-				}
-				else
-				{
-					print("trying to go DOWN from middle of rotation");
-					// start from current, end at -90
-					rotateStart = currRotAngle;
-					rotateEnd = -90.0f;
-					// should be the same speed as normal
-					duration = Math.Abs(rotateEnd - rotateStart) / defaultRotSpeed;
-				}
-			}
-			else // NOT currently rotating
-			{
-				shouldRotate = true;
-
-				if (state)
-				{
-					print("Rotating UP from standstill");
-					// start from -90, end at 0
-					rotateStart = -90.0f;
-					rotateEnd = 0.0f;
-					duration = defaultDuration;
-				}
-				else
-				{
-					print("Rotating DOWN from standstill");
-					// start from 0, end at -90
-					rotateStart = 0.0f;
-					rotateEnd = -90.0f;
-					duration = defaultDuration;
-				}
+				// the direction should be the OPPOSITE of the direction needed for activation
+				rotationDirection = -activationRotationDirection;
 			}
 		}
 
-		void PerformRotation()
+		void PerformRotationZ()
 		{
-			float value = (timer - rotateStartTime) / duration;
-			if (0 <= value && value < 1)
+			TS.GetRotation(this.ID, out vec3 rot);
+			float amountComplete = InverseLerp(rotateStartAngle, rotateEndAngle, rot.z);
+
+			if (amountComplete < 1)
 			{
-				float rotAngle = MathF.Lerp(rotateStart, rotateEnd, value);
-				TS.SetRotation(this.ID, new vec3(0, 0, rotAngle));
+				rot.z += rotationSpeed * Time.deltaTime * rotationDirection;
+				TS.SetRotation(this.ID, rot);
 			}
-			else // when the rotation is complete
+			else // if (amountComplete >= 1)
 			{
 				shouldRotate = false;
-				// if the platform has no rotation, it's active
-				platformState = rotateEnd == 0;
+				float angle = NiceRotationAngle(rot.z);
+				platformState = angle == platformActiveAngle;
 			}
+		}
+
+		/*
+		void Swap(ref float a, ref float b)
+		{
+			// float temp = a;
+			// a = b;
+			// b = temp;
+			(b, a) = (a, b);
+		}
+		*/
+
+		float InverseLerp(float start, float end, float output)
+		{
+			// r = p + (t)(q-p) <- Lerp formula
+			// r - p = (t)(q-p)
+			// t = (r - p) / (q - p)
+			return (output - start) / (end - start);
+		}
+
+		void TrembleZ()
+		{
+			const float amplitude = 0.125f;
+			const float frequency = 60.0f;
+
+			TS.GetPosition(this.ID, out vec3 pos);
+			pos.z = originalPosition.z + amplitude * MathF.Sin(timer * frequency);
+			TS.SetPosition(this.ID, pos);
 		}
 	}
 }
