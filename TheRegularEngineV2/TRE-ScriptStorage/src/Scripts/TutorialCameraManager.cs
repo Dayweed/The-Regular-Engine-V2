9722c1362045c416c0c3556ade991ec3e356ff16
zhengrongg <zhengrong29@gmail.com>
2024-03-11 11:30:39 +0800

    scene got corrupted

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/TutorialCameraManager.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/TutorialCameraManager.cs
@@ -77,8 +77,8 @@ namespace TRE
 			preTransitions.Init();
 			preTransitions.AddCameraData(new vec3(180, 50, -430), new vec3(30, 230, 0), 5f);
 			preTransitions.AddCameraData(new vec3(180, 50, -20), new vec3(30, 270, 0), 10f);
-			preTransitions.AddCameraData(new vec3(30, 40, -10), new vec3(30, 180, 0), 6f);
-			preTransitions.AddCameraData(new vec3(0, 10, 30), new vec3(30, 180, 0), 5f);
+			preTransitions.AddCameraData(new vec3(30, 40, -10), new vec3(30, 180, 0), 5.5f);
+			preTransitions.AddCameraData(new vec3(0, 10, 30), new vec3(30, 180, 0), 4f);
 		}
 		public void Update()
 		{
