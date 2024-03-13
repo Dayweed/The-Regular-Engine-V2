a6a5355967735845e4bdb82d443f0dbed7993177
zhengrongg <zhengrong29@gmail.com>
2024-03-13 23:39:24 +0800

    bruh camera

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/TutorialCameraManager.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/TutorialCameraManager.cs
@@ -75,6 +75,7 @@ namespace TRE
 
 			preTransitions = new CameraTransitions();
 			preTransitions.Init();
+			preTransitions.AddCameraData(new vec3(180, 50, -430), new vec3(30, 230, 0), 0.5f);
 			preTransitions.AddCameraData(new vec3(180, 50, -430), new vec3(30, 230, 0), 4f);
 			preTransitions.AddCameraData(new vec3(180, 50, -20), new vec3(30, 270, 0), 8f);
 			preTransitions.AddCameraData(new vec3(30, 40, -10), new vec3(30, 180, 0), 4f);
