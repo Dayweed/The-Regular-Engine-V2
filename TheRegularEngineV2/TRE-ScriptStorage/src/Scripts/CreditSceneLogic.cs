bcb90039845f9cc4a43b2628c411dcc02edfa6e0
Pancakes <jerelpeck99@gmail.com>
2024-03-15 13:51:37 +0800

    Done

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/CreditSceneLogic.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/CreditSceneLogic.cs
@@ -7,27 +7,15 @@ namespace TRE
         Entity mCreditText;
         float m_ScrollSpeed = 100f;
 
-        Entity mHoley;
-        Entity mMoley;
-        public vec3 JumpVelocity;
-
         public void Start()
         {
             mCreditText = ECSManager.FindEntityByName("Credits");
-            mHoley = ECSManager.FindEntityByName("Holey");
-            mMoley = ECSManager.FindEntityByName("Moley");
-            JumpVelocity = new vec3(0, 70, 0);
         }
 
         public void Update()
         {
             ScrollingCredits();
 
-            if (InputSystem.GetKeyTriggered(InputKeys.S))
-            {
-                PhysicsSystem.AddForce(mHoley.ID, JumpVelocity, ForceMode.VelocityChange);
-                PhysicsSystem.AddForce(mMoley.ID, JumpVelocity, ForceMode.VelocityChange);
-            }
         }
         public void ScrollingCredits()
         {
