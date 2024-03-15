6c496c8f3d610b8dfb6a125cddfe202eeb0b24b5
Pancakes <jerelpeck99@gmail.com>
2024-03-15 12:59:59 +0800

    Credits scene scrolling

---
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/CreditSceneLogic.cs
@@ -0,0 +1,23 @@
+﻿using GlmSharp;
+using System.Collections.Generic;
+
+namespace TRE
+{
+    public class CreditSceneLogic : Entity
+    {
+        Entity mCreditText;
+        float m_ScrollSpeed = 100f;
+
+        public void Start()
+        {
+            mCreditText = ECSManager.FindEntityByName("Credits");
+        }
+
+        public void Update()
+        {
+            vec3 currentpos = mCreditText.GetComponent<Transform>().Position;
+            currentpos.y -= Time.GetDeltaTime() * m_ScrollSpeed;
+            TransformSystem.SetPosition(mCreditText.ID, currentpos);
+        }
+    }
+}
