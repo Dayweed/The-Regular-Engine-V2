583915fe09f5d62838e6a6281e642df646a443bf
Pancakes <jerelpeck99@gmail.com>
2024-03-15 13:32:26 +0800

    CreditsScene add Moles

--- TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/CreditSceneLogic.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/CreditSceneLogic.cs
@@ -1,5 +1,4 @@
 ﻿using GlmSharp;
-using System.Collections.Generic;
 
 namespace TRE
 {
@@ -8,12 +7,29 @@ namespace TRE
         Entity mCreditText;
         float m_ScrollSpeed = 100f;
 
+        Entity mHoley;
+        Entity mMoley;
+        public vec3 JumpVelocity;
+
         public void Start()
         {
             mCreditText = ECSManager.FindEntityByName("Credits");
+            mHoley = ECSManager.FindEntityByName("Holey");
+            mMoley = ECSManager.FindEntityByName("Moley");
+            JumpVelocity = new vec3(0, 70, 0);
         }
 
         public void Update()
+        {
+            ScrollingCredits();
+
+            if (InputSystem.GetKeyTriggered(InputKeys.S))
+            {
+                PhysicsSystem.AddForce(mHoley.ID, JumpVelocity, ForceMode.VelocityChange);
+                PhysicsSystem.AddForce(mMoley.ID, JumpVelocity, ForceMode.VelocityChange);
+            }
+        }
+        public void ScrollingCredits()
         {
             vec3 currentpos = mCreditText.GetComponent<Transform>().Position;
             currentpos.y -= Time.GetDeltaTime() * m_ScrollSpeed;
