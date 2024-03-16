74adb90dce0835a8436a436e18decf47299e2027
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2024-03-16 23:24:24 +0800

    added tutorial diorama for main menu

---
+++ TheRegularEngineV2/TRE-ScriptStorage/src/Scripts/DioramaSpin.cs
@@ -0,0 +1,17 @@
+using GlmSharp;
+
+namespace TRE
+{
+	using TS = TransformSystem;
+	public class DioramaSpin : Entity
+	{
+		const float rotSpeed = 50.0f;
+
+		void Update()
+		{
+			TS.GetRotation(this.ID, out vec3 rot);
+			rot.y += rotSpeed * Time.deltaTime;
+			TS.SetRotation(this.ID, rot);
+		}
+	}
+}
