e8b200d2e55ed62d53a0d31eee122c7dc7c02f89
Wee Yi <tanweeyi650@hotmail.com>
2024-03-28 21:52:32 +0800

    Start screen to have UI for controller

--- TheRegularEngineV2/TRE-ScriptStorage/Scripts/Scene Logic/MainMenuSceneTransition.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/Scripts/Scene Logic/MainMenuSceneTransition.cs
@@ -1,4 +1,6 @@
-﻿namespace TRE
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace TRE
 {
 	using AS = AudioSystem;
 	using IS = InputSystem;
@@ -9,7 +11,12 @@
 		//private string ControlsDisplaySceneName;
 		public bool spacePressed;
 		public bool enterPressed;
-		public bool ContollerAPressed;
+		public bool ControllerAPressed;
+		public bool ControllerBPressed;
+
+        public bool ControllerConnected;
+        private Entity StartText;
+		private bool changeUI = false;
 
 		public void Start()
 		{
@@ -17,10 +24,53 @@
 			//ControlsDisplaySceneName = "ControlsDisplay";
 			spacePressed = false;
 			enterPressed = false;
+			ControllerAPressed = false;
+			ControllerBPressed = false;
+
+            StartText = ECSManager.FindEntityByName("Start_Text");
+
+            if (IS.GetControllerConnected(0) || IS.GetControllerConnected(1))
+            {
+                ControllerConnected = true;
+				// change the texture for the ui display
+				StartText.GetComponent<SpriteRenderer>().Texture = "ui-start-controller.png";
+
+            }
+            else
+            {
+                ControllerConnected = false;
+            }
 		}
 
 		public void Update()
 		{
+
+			// check for controller hotplug
+            if ((IS.GetControllerConnected(0) || IS.GetControllerConnected(1)) && changeUI == false)
+            {
+                ControllerConnected = true;
+				changeUI = true;
+            }
+            else if((!IS.GetControllerConnected(0) || !IS.GetControllerConnected(1)) && changeUI == false)
+            {
+                ControllerConnected = false;
+                changeUI = true;
+            }
+
+            if (changeUI)
+            {
+                if (ControllerConnected)
+                {
+                    StartText.GetComponent<SpriteRenderer>().Texture = "ui-start-controller.png";
+                }
+                else
+                {
+                    StartText.GetComponent<SpriteRenderer>().Texture = "ui-start.png";
+                }
+                changeUI = false;
+            }
+
+
 			// Close Game
 			if (IS.GetKeyHold(InputKeys.Escape))
 			{
@@ -41,12 +91,20 @@
 					AS.Play(13376208322872696703);
 			}
 
-			if (IS.GetControllerButtonPress(0, IS.Button.A))
+			if (IS.GetControllerButtonTriggered(0, IS.Button.A) || IS.GetControllerButtonTriggered(1, IS.Button.A))
 			{
-				ContollerAPressed = true;
+				ControllerAPressed = true;
+                if (ECSManager.IsValidEntity(13376208322872696703))
+                    AS.Play(13376208322872696703);
 			}
+            if (IS.GetControllerButtonTriggered(0, IS.Button.B) || IS.GetControllerButtonTriggered(1, IS.Button.B))
+            {
+                ControllerBPressed = true;
+                if (ECSManager.IsValidEntity(13376208322872696703))
+                    AS.Play(13376208322872696703);
+            }
 
-			if ((spacePressed && enterPressed) || ContollerAPressed)
+			if ((spacePressed && enterPressed) || (ControllerAPressed && ControllerBPressed))
 			{
 				if (ECSManager.IsValidEntity(6154957411926925810))
 					AS.Play(6154957411926925810);
@@ -55,6 +113,8 @@
 
 				spacePressed = false;
 				enterPressed = false;
+				ControllerAPressed = false;
+				ControllerBPressed = false;
 			}
 		}
 	}
