fbfbf5fc345e037a33c68c5b8784bbce09ed60c4
Pancakes <jerelpeck99@gmail.com>
2024-03-31 23:55:05 +0800

    Compile skybox texture and set level 2 skybox

--- TheRegularEngineV2/TRE-ScriptStorage/Scripts/Tools/SkyboxSetter.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/Scripts/Tools/SkyboxSetter.cs
@@ -5,7 +5,17 @@
 
 		public void Start()
 		{
-			RendererSystem.Engine_SetSkyboxEnvironment("skybox1-desert.png", "skybox2-desert.png", "skybox3-desert.png", "skybox4-desert.png", "skybox5-desert.png", "skybox6-desert.png");
+			string currentscene = Scene.GetSceneName();
+
+            if (currentscene == "Level_1")
+            {
+				RendererSystem.Engine_SetSkyboxEnvironment("skybox1-desert.png", "skybox2-desert.png", "skybox3-desert.png", "skybox4-desert.png", "skybox5-desert.png", "skybox6-desert.png");
+            }
+
+			if (currentscene == "Level_2")
+			{
+                RendererSystem.Engine_SetSkyboxEnvironment("skybox1-l2.png", "skybox2-l2.png", "skybox3-l2.png", "skybox4-l2.png", "skybox5-l2.png", "skybox6-l2.png");
+            }
 		}
 	}
 }
