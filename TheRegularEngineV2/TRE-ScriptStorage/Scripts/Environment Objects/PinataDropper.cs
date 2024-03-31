e74332c5bdb1a86fc1872ebd3c69430520d1ea94
PocketsPocket <royceleewh@gmail.com>
2024-04-01 01:22:44 +0800

    pushing for mr pancakes

--- TheRegularEngineV2/TRE-ScriptStorage/Scripts/Environment Objects/PinataDropper.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/Scripts/Environment Objects/PinataDropper.cs
@@ -56,19 +56,33 @@ namespace TRE
 			Pinata14 = ECSManager.FindEntityByName("Pinata_21");
 
 			Pinata1.SetActive(false);
+			Pinata1.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata2.SetActive(false);
+			Pinata2.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata3.SetActive(false);
+			Pinata3.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata4.SetActive(false);
+			Pinata4.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata5.SetActive(false);
+			Pinata5.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata6.SetActive(false);
+			Pinata6.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata7.SetActive(false);
+			Pinata7.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata8.SetActive(false);
+			Pinata8.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata9.SetActive(false);
+			Pinata9.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata10.SetActive(false);
+			Pinata10.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata11.SetActive(false);
+			Pinata11.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata12.SetActive(false);
+			Pinata12.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata13.SetActive(false);
+			Pinata13.GetComponent<CapsuleCollider>().IsActive = false;
 			Pinata14.SetActive(false);
+			Pinata14.GetComponent<CapsuleCollider>().IsActive = false;
 		}
 
 		public void Update()
@@ -78,19 +92,33 @@ namespace TRE
 			if (spawnRegion)
 			{
 				Pinata1.SetActive(true);
+				Pinata1.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata2.SetActive(true);
+				Pinata2.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata3.SetActive(true);
+				Pinata3.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata4.SetActive(true);
+				Pinata4.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata5.SetActive(true);
+				Pinata5.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata6.SetActive(true);
+				Pinata6.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata7.SetActive(true);
+				Pinata7.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata8.SetActive(true);
+				Pinata8.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata9.SetActive(true);
+				Pinata9.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata10.SetActive(true);
+				Pinata10.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata11.SetActive(true);
+				Pinata11.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata12.SetActive(true);
+				Pinata12.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata13.SetActive(true);
+				Pinata13.GetComponent<CapsuleCollider>().IsActive = true;
 				Pinata14.SetActive(true);
+				Pinata14.GetComponent<CapsuleCollider>().IsActive = true;
 			}
 				
 		}
