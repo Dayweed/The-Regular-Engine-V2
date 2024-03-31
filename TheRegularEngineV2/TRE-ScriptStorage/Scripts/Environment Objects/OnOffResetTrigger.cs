8563a5ca800fd656d5e600b86f9dddf63215deea
PocketsPocket <royceleewh@gmail.com>
2024-03-31 21:41:18 +0800

    moving cameras but sort of playable

--- TheRegularEngineV2/TRE-ScriptStorage/Scripts/Environment Objects/OnOffResetTrigger.cs
+++ TheRegularEngineV2/TRE-ScriptStorage/Scripts/Environment Objects/OnOffResetTrigger.cs
@@ -4,11 +4,13 @@ namespace TRE
 	{
 		OnOffPlatformManager section2Manager;
 		OnOffPlatformManager section4Manager;
+		OnOffPlatformManager section3Manager;
 
 		public void Start()
 		{
 			section2Manager = ECSManager.FindEntityByName("OnOff_Platform_Manager_2").GetComponent<OnOffPlatformManager>();
 			section4Manager = ECSManager.FindEntityByName("OnOff_Platform_Manager_4").GetComponent<OnOffPlatformManager>();
+			section3Manager = ECSManager.FindEntityByName("OnOff_Platform_Manager_3").GetComponent<OnOffPlatformManager>();
 		}
 
 		public void OnTriggerStay(System.UInt64 otherID)
@@ -18,6 +20,7 @@ namespace TRE
 			{
 				section2Manager.MakeAllPlatformsInactive();
 				section4Manager.MakeAllPlatformsInactive();
+				section3Manager.MakeAllPlatformsInactive();
 			}
 		}
 	}
